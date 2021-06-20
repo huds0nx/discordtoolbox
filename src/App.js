eb79a1d9eec54efb57b7ebeaf72cb535e2cfe01f
hudson <39014019+huds0nx@users.noreply.github.com>
2021-06-19 22:03:47 -0400

    finished webhook deleter and cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,149 @@
 import React from 'react';
 import clsx from 'clsx';
-import { Drawer, Toolbar, CssBaseline, AppBar, List, Typography, Divider, IconButton, ListItem, ListItemIcon, ListItemText, InputBase } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Drawer, Toolbar, CssBaseline, AppBar, List, Typography, Divider, IconButton, ListItem, ListItemIcon, Collapse, ListItemText, InputBase } from '@material-ui/core';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import SearchIcon from '@material-ui/icons/Search';
-import { useStyles } from './components/styles'
-import { useTheme } from '@material-ui/core/styles'
+import { Delete, Edit, Send, Comment, Search, Menu } from '@material-ui/icons';
+import { fade, useTheme, makeStyles } from '@material-ui/core/styles'
 import Deleter from './components/webhook/deleter'
+
+const drawerWidth = 240;
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: '#20212f',
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  // search bar
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+  // side menu
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+  // nested list
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+  // webhook deleter
+  deleter: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(1, 1, 1, 0),
+  },
+  header: {
+    position: 'absolute',
+    textAlign: 'center',
+    fontSize: '2em',
+    top: 0,
+  }
+}));
 
 function App() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const handleOpenClose = () => {
+    setOpen(!open);
   };
 
   return (
@@ -37,18 +159,18 @@ function App() {
             <IconButton
               color="inherit"
               aria-label="Open sidebar menu"
-              onClick={handleDrawerOpen}
+              onClick={handleOpenClose}
               edge="start"
               className={clsx(classes.menuButton, open && classes.hide)}
             >
-              <MenuIcon />
+              <Menu />
             </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             DiscordToolbox
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
-              <SearchIcon />
+              <Search />
             </div>
             <InputBase
               placeholder="Search…"
@@ -71,27 +193,50 @@ function App() {
         }}
       >
         <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
+          <IconButton onClick={handleOpenClose}>
             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </div>
         <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <List disablePadding>
+          <ListItem button>
+            <ListItemIcon>
+              <Search />
+            </ListItemIcon>
+            <ListItemText primary="Discord ID" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemIcon>
+              <Comment />
+            </ListItemIcon>
+            <ListItemText primary="Webhooks" />
+          </ListItem>
+            <List disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon>
+                  <Send />
+                </ListItemIcon>
+                <ListItemText primary="Sender" />
+              </ListItem>
+            </List>
+            <List disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon>
+                  <Edit />
+                </ListItemIcon>
+                <ListItemText primary="Editer" />
+              </ListItem>
+            </List>
+            <List disablePadding>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon>
+                  <Delete />
+                </ListItemIcon>
+                <ListItemText primary="Deleter" />
+              </ListItem>
+              <Divider />
+            </List>
         </List>
       </Drawer>
       <main
@@ -100,10 +245,14 @@ function App() {
         })}
       >
         <div className={classes.drawerHeader} />
+        <header className={classes.header}>
+            Webhook Deleter
+        </header>
+        <div className={classes.deleter}>
+          <Deleter />
+        </div>
+        <Divider />
       </main>
-      <div className="classes.webhook">
-        <Deleter />
-      </div>
     </div>
   );
 }
--- src/components/webhook/deleter.js
+++ src/components/webhook/deleter.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
+    display: 'flex',
+    alignItems: 'center',
+  },
+  textField: {
+    flex: 1,
+    paddingRight: 20,
+    width: 500,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
   },
 }));
 
 export default function Deleter() {
+  function deleteWebhook() {
+    const request = new XMLHttpRequest();
+    request.open("DELETE", textRef.current.value);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send();
+  }
+
   const classes = useStyles();
+  const textRef = useRef();
+
+
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-    </form>
+    <div className={classes.deleter}>
+      <TextField 
+        className={classes.textField} 
+        inputRef={textRef}
+        placeholder="Webhook URL"
+      />
+      <Button onClick={deleteWebhook} variant="contained" color="primary">
+        Delete
+      </Button>
+    </div>
   );
 }
